30bef46163dd7b9dcab0332812ed05448edf04a2
vsemenkov <vsemenkow@gmail.com>
2022-06-08 12:39:44 +0300

    Singleton

--- Singleton.java
+++ Singleton.java
@@ -5,14 +5,14 @@ public class Singleton {
     public static final Singleton INSTANCE = new Singleton();
 
     //private static Singleton instance;
-    private Singleton(){
+    private Singleton() {
 
     }
 
-  //  public static Singleton getInstance() {
-   //     if (instance == null) {
+    //  public static Singleton getInstance() {
+    //     if (instance == null) {
     //        instance = new Singleton();
-        }
-     //   return instance;
-   // }
-
+    //}
+    //   return instance;
+    // }
+}
